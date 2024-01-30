7fc9e1c2841999c4cc2514b0b771ead289f64bb7
alesof <al.sofia@studenti.unina.it>
2024-01-30 12:55:52 +0100

    Clean
    Everything is now stored in the src folder in order to use the cloned folder as ws.

--- ros2_driver_setup.sh
+++ ros2_driver_setup.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]; then
-  echo "Error: Please run the script with sudo."
+if [ "$EUID" -eq 0 ]; then
+  echo "Error: Please don't run the script with sudo."
   exit 1
 fi
 
 echo "Sourcing ROS2 environment..."
 source /opt/ros/humble/setup.bash
 
-echo "Creating a new ROS2 workspace..."
-mkdir -p ~/eth_ros2_ws/src
-cd ~/eth_ros2_ws/src
+# Assuming the script is located one level above 'src'
+ROS2_WS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SRC_DIR="$ROS2_WS_DIR/src"
+
+echo "Using current ROS2 workspace in: $ROS2_WS_DIR"
+
+# No need to create a new workspace, already in the desired location
+cd "$SRC_DIR"
 
 if [ -d "ethercat_driver_ros2" ]; then
   echo "ethercat_driver_ros2 repository already exists. Skipping cloning."
@@ -19,17 +24,16 @@ else
   git clone https://github.com/ICube-Robotics/ethercat_driver_ros2.git
 fi
 
+cd "$ROS2_WS_DIR"
 
 echo "Installing ROS2 dependencies..."
-cd ~/eth_ros2_ws
 rosdep install --ignore-src --from-paths . -y -r
 
 echo "Building ROS2 workspace..."
 colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release --symlink-install
 
 echo "Sourcing the ROS2 workspace..."
-source install/setup.bash
+source "$ROS2_WS_DIR/install/setup.bash"
 
 echo "ROS2 environment setup and workspace creation completed."
 exit 0
-
